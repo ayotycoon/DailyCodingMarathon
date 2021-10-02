59f6c87f64ddde6aac65f394549b35f23f64d2e4
ayotycoon <kaysehventeen@gmail.com>
2021-10-01 23:02:53 -0500

    c

---
+++ src/LengthLongestPath_Oct1_2021/LengthLongestPath_Ayokunle.java
@@ -0,0 +1,12 @@
+package LengthLongestPath_Oct1_2021;
+
+// 	https://leetcode.com/problems/longest-absolute-file-path
+public class LengthLongestPath_Ayokunle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+
+
+}
