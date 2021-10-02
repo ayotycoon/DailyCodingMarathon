ee3b59cb3d8b6d7ac4239aa0d61ee872912a5676
ayotycoon <kaysehventeen@gmail.com>
2021-10-01 23:44:35 -0500

    lieele refactor

--- src/LengthLongestPath_Oct1_2021/LengthLongestPath_Ayokunle.java
+++ src/LengthLongestPath_Oct1_2021/LengthLongestPath_Ayokunle.java
@@ -4,69 +4,37 @@ import java.util.HashMap;
 
 // 	https://leetcode.com/problems/longest-absolute-file-path
 public class LengthLongestPath_Ayokunle {
+    public static int lengthLongestPath(String input) {
+        if(input == null || input.length() == 0) return 0;
+        int max = 0;
+        var split = input.split("\\n");
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for(int i =0; i < split.length; i++){
+            String item = split[i];
+            // System.out.println(item);
+            boolean isFile =item.contains(".");
 
+            if(i ==0 && !isFile){
+                // then its root
 
+                map.put(i,item.length());
+            }else {
+                var x = item.split("\\t");
+                String alt = x[x.length-1];
+                int no = item.lastIndexOf("\t")+1;
+                int prev = map.containsKey(no-1) ? map.get(no -1) : 0;
 
-        private boolean checkIfFile(String s){
-
-            for(int i =0; i < s.length(); i++){
-                char item = s.charAt(i);
-                if( item == '.'){
-                    return true;
-                }
-            }
-
-            return false;
-
-        }
-        public int lengthLongestPath(String input) {
-            if(input == null || input.length() == 0) return 0;
-
-            int max = 0;
-            var split = input.split("\\n");
-
-            // System.out.println(Arrays.toString(split));
-
-            HashMap<Integer, Integer> map = new HashMap<>();
-
-            for(int i =0; i < split.length; i++){
-                String item = split[i];
-                // System.out.println(item);
-                boolean isFile = checkIfFile(item);
-
-                if(i ==0 && !isFile){
-                    // then its root
-
-                    map.put(i,3);
+                if(isFile){
+                    max = Math.max(prev  + (prev == 0 ? 0 :1) + alt.length(),max);
                 }else {
-                    var x = item.split("\\t");
-                    String alt = x[x.length-1];
-
-                    int no = item.length() - alt.length();
-                    int prev = map.containsKey(no-1) ? map.get(no -1) : 0;
-
-
-                    // check if its a folder
-
-                    if(isFile){
-                        max = Math.max(prev  + alt.length(),max);
-                    }else {
-
-                        map.put( no, prev + 1 + alt.length() );
-                    }
-
+                    map.put( no, prev + (prev == 0 ? 0 :1) + alt.length() );
                 }
-
-
             }
-
-            return max;
-
-
         }
 
-
+        return max;
+    }
 
 
 
@@ -74,8 +42,7 @@ public class LengthLongestPath_Ayokunle {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-
+        lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext");
     }
 
 
